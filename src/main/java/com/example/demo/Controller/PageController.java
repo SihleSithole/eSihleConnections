7b22a096fcca299c9ca5885f251883e858d00012
SihleSithole <sihlesithole810@gmail.com>
2024-10-15 20:45:42 +0200

    eSihleConnections datasource

---
+++ src/main/java/com/example/demo/Controller/PageController.java
@@ -0,0 +1,16 @@
+package com.example.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+	
+	 @GetMapping("/")
+     public String homePage() {
+	
+		 return "eSihleConnections.html";
+		 
+	 }
+
+}
---
+++ src/main/java/com/example/demo/ESihleConnectionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESihleConnectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESihleConnectionsApplication.class, args);
+	}
+
+}
